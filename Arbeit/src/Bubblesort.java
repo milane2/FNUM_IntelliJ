d1f9eb4d2b7a574e0db9d9fdb99ee7bc09eb9170
milane <a@b.de>
2021-05-20 12:29:33 +0200

    Bubblesort anfang

--- Arbeit/src/Bubblesort.java
+++ Arbeit/src/Bubblesort.java
@@ -1,8 +1,14 @@
+import java.io.IOException;
+
 public class Bubblesort {
 
-    public static void main(String[] args){
+    public static void main(String[] args) throws IOException {
 
-        int a[];
+        System.out.println("Wie viele Zahlen sollen sortiert werden:");
+        int anzahl = IO.readInteger();
+        int a[] = new int[anzahl];
+
+
 
     }
 }
