d0a41b5a5d010d7c148cf8d45d06ce0124d29e56
milane2 <milane2@schule.bremen.de>
2021-06-17 14:37:14 +0200

    kino

--- Arrays/src/Kino.java
+++ Arrays/src/Kino.java
@@ -4,11 +4,19 @@ public class Kino {
 
     public static void main(String[] args) throws IOException {
 
+
         System.out.println("Nennen sie die Anzahl an Reihen: ");
-        int Reihe = IO.readInteger();
+        int reihe = IO.readInteger();
 
         System.out.println("Nennen sie die Anzahl an Sitze pro Reihe: ");
         int sitzProReihe = IO.readInteger();
+    }
+    public static String erzeugeSaal() {
+
+        int[][] sitze = new int[reihe][sitzProReihe];
+
 
     }
+
+
 }
